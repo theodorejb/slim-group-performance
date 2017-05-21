da1d1dc4b1cea81d352e60752463c1a048b2ccbd
Theodore Brown <theodorejb@outlook.com>
2017-05-21 10:06:40 -0500

    Test using a static handler function and passing the entity class via `setArgument`

--- init.php
+++ init.php
@@ -10,14 +10,17 @@ use Slim\Http\Response;
 
 require 'vendor/autoload.php';
 
+$startTime = microtime(true);
+$initTime = 0;
+$totalRoutes = 0;
+
 function initRoutes($base, $generateUnmatchableRoutes = true)
 {
-    $startTime = microtime(true);
+    global $startTime, $initTime;
+
     $app = new App();
     $groupCount = 50;
     $subgroupCount = 25;
-    $initTime = 0;
-    $totalRoutes = 0;
 
     for ($g = 1; $g <= $groupCount; $g++) {
         $app->group("/$base/group{$g}", function () use ($app, $g, $generateUnmatchableRoutes, $groupCount, $subgroupCount, $startTime, &$initTime, &$totalRoutes) {
@@ -30,7 +33,7 @@ function initRoutes($base, $generateUnmatchableRoutes = true)
             }
 
             for ($s = 1; $s <= $subgroupCount; $s++) {
-                $app->group("/sub{$s}", endpointFactory($app, $g, $s, $groupCount, $startTime, $initTime, $totalRoutes));
+                $app->group("/sub{$s}", endpointFactory($app, $g, $s));
             }
         });
     }
@@ -39,34 +42,35 @@ function initRoutes($base, $generateUnmatchableRoutes = true)
     $app->run();
 }
 
-function endpointFactory(App $app, $g, $s, $groupCount, $startTime, &$initTime, &$totalRoutes)
+function endpointFactory(App $app, $g, $s)
 {
+    global $totalRoutes;
+
     $totalRoutes += 6;
     // A real world app would often use an ORM to generate get/post/patch/update/delete methods for each endpoint.
     // The ORM would be passed a unique class name for each subgroup, which it would use inside route callbacks.
     $class = "class" . md5($g * $s);
 
-    return function () use ($app, $class, $groupCount, $startTime, &$initTime, &$totalRoutes) {
-        $app->get('', routeFactory($class, 'search', $groupCount, $startTime, $initTime, $totalRoutes));
-        $app->get('/{id}', routeFactory($class, 'get by ID', $groupCount, $startTime, $initTime, $totalRoutes));
-        $app->post('', routeFactory($class, 'create', $groupCount, $startTime, $initTime, $totalRoutes));
-        $app->put('/{id}', routeFactory($class, 'update by ID', $groupCount, $startTime, $initTime, $totalRoutes));
-        $app->patch('/{id}', routeFactory($class, 'patch by ID', $groupCount, $startTime, $initTime, $totalRoutes));
-        $app->delete('/{id}', routeFactory($class, 'delete by ID', $groupCount, $startTime, $initTime, $totalRoutes));
+    return function () use ($app, $class) {
+        $app->get('', 'handler')->setArgument('entity', $class);
+        $app->get('/{id}', 'handler')->setArgument('entity', $class);
+        $app->post('', 'handler')->setArgument('entity', $class);
+        $app->put('/{id}', 'handler')->setArgument('entity', $class);
+        $app->patch('/{id}', 'handler')->setArgument('entity', $class);
+        $app->delete('/{id}', 'handler')->setArgument('entity', $class);
     };
 }
 
-function routeFactory($class, $route, $groupCount, $startTime, &$initTime, &$totalRoutes)
+// in a real-world app there would be separate delete/patch/update/etc. handler functions
+function handler(Request $request, Response $response, array $args)
 {
-    return function (Request $request, Response $response, array $args) use ($class, $route, $groupCount, $startTime, &$initTime, &$totalRoutes) {
-        return $response->withJson([
-            'class' => $class,
-            'route' => $route,
-            'args' => $args,
-            'totalGroups' => $groupCount,
-            'generatedRoutes' => $totalRoutes,
-            'initTime' => $initTime,
-            'responseTime' => microtime(true) - $startTime,
-        ]);
-    };
+    global $totalRoutes, $startTime, $initTime;
+
+    return $response->withJson([
+        'class' => $request->getAttribute('entity'),
+        'args' => $args,
+        'generatedRoutes' => $totalRoutes,
+        'initTime' => $initTime,
+        'responseTime' => microtime(true) - $startTime,
+    ]);
 }
